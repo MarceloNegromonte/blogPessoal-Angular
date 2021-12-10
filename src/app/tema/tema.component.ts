75d22d6307b1b10553bbd0d9bfcd126e86b055c4
MarceloNegromonte <mjncavalcante@gmail.com>
2021-12-10 10:58:54 -0300

    get-pos-tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,16 +11,36 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit(){
-
     if(environment.token == ''){
       alert('Sua seção expirou, faça o login novamente.')
       this.router.navigate(['/entrar'])
     }
+
+    this.buscarTemas()
+  }
+
+  buscarTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Tema[])=> {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=> {
+      this.tema = resp;
+      alert('Tema cadastrado')
+      this.buscarTemas()
+      this.tema = new Tema()
+    })
   }
 
 }
