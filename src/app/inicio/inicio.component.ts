f809072d91a450213b3f016c90719b1183c952a3
MarceloNegromonte <mjncavalcante@gmail.com>
2021-12-13 11:50:19 -0300

    Implementação do post e get de postagens

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,13 @@
+import { identifierName } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -8,10 +15,21 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
 
+  idUsuario = environment.id
+  usuario: Usuario = new Usuario()
+
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private temaService: TemaService,
+    private postagemService: PostagemService,
+    private authService: AuthService
+    ) { }
 
   ngOnInit(){
 
@@ -19,6 +37,47 @@ export class InicioComponent implements OnInit {
       alert('Sua seção expirou, faça o login novamente.')
       this.router.navigate(['/entrar'])
     }
+    
+    this.buscarTemas()
+    this.buscarPostagens()
   }
 
+  buscarTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Tema[])=> {
+      this.listaTemas = resp
+    })
+  }
+
+  getTemaById(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema=resp
+    })
+  }
+
+  buscarPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=> {
+      this.listaPostagens = resp
+    })
+  }
+
+  findByIdUsuario(){
+    this.authService.getByIdUsuario(this.idUsuario).subscribe((resp: Usuario)=>{
+      this.usuario = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.usuario.id = this.idUsuario
+    this.postagem.criador = this.usuario
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      alert('Postagem feita!')
+      this.postagem = new Postagem()
+      this.buscarPostagens()
+    })
+  }
 }
