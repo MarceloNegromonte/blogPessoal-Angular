03c9e5eb0442c2d2b7a24959a6fa3a3d5c8a0571
MarceloNegromonte <mjncavalcante@gmail.com>
2021-12-14 09:36:56 -0300

    Implementação rota HEROKU

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -15,15 +15,15 @@ export class AuthService {
   ) { }
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
-    return this.http.post<UsuarioLogin>('http://localhost:8080/usuario/logar', usuarioLogin)
+    return this.http.post<UsuarioLogin>('https://api-blogpessoal.herokuapp.com/usuario/logar', usuarioLogin)
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
-    return this.http.post<Usuario>('http://localhost:8080/usuario/cadastrar', usuario)
+    return this.http.post<Usuario>('https://api-blogpessoal.herokuapp.com/usuario/cadastrar', usuario)
   }
 
   getByIdUsuario(id: number): Observable<Usuario>{
-    return this.http.get<Usuario>(`http://localhost:8080/usuario/${id}`)
+    return this.http.get<Usuario>(`https://api-blogpessoal.herokuapp.com/usuario/${id}`)
   }
 
   logado(){
